7863ddf9474e44c082bc7095f6ebbf10ac7c7a0e
droidbot09 <126750520+droidbot09@users.noreply.github.com>
2024-01-09 21:33:15 +0800

    Update banner.py

--- banner.py
+++ banner.py
@@ -1,10 +1,10 @@
 from colorama import Fore, Style
 
 banner = f"""
-{Fore.BLUE + Style.BRIGHT}███╗   ██╗ ██████╗ ██╗      ███╗   ███╗███████╗ ██████╗{Style.RESET_ALL}
-{Fore.YELLOW + Style.BRIGHT}████╗  ██║██╔════╝ ██║      ████╗ ████║██╔════╝██╔════╝{Style.RESET_ALL}
-{Fore.BLUE + Style.BRIGHT}██╔██╗ ██║██║  ███╗██║█████╗██╔████╔██║███████╗██║  ███╗{Style.RESET_ALL}
-{Fore.YELLOW + Style.BRIGHT}██║╚██╗██║██║   ██║██║╚════╝██║╚██╔╝██║╚════██║██║   ██║{Style.RESET_ALL}
-{Fore.BLUE + Style.BRIGHT}██║ ╚████║╚██████╔╝███████╗ ██║ ╚═╝ ██║███████║╚██████╔╝{Style.RESET_ALL}
-{Fore.YELLOW + Style.BRIGHT}╚═╝  ╚═══╝ ╚═════╝ ╚══════╝ ╚═╝     ╚═╝╚══════╝ ╚═════╝{Style.RESET_ALL}
+{Fore.CYAN + Style.BRIGHT}███╗   ██╗ ██████╗ ██╗      ███╗   ███╗███████╗██████╗{Style.RESET_ALL}
+{Fore.MAGENTA + Style.BRIGHT}████╗  ██║██╔════╝ ██║      ████╗ ████║██╔════╝██╔══██╗{Style.RESET_ALL}
+{Fore.CYAN + Style.BRIGHT}██╔██╗ ██║██║  ███╗██║█████╗██╔████╔██║███████╗██║  ██║{Style.RESET_ALL}
+{Fore.MAGENTA + Style.BRIGHT}██║╚██╗██║██║   ██║██║╚════╝██║╚██╔╝██║╚════██║██║  ██║{Style.RESET_ALL}
+{Fore.CYAN + Style.BRIGHT}██║ ╚████║╚██████╔╝███████╗ ██║ ╚═╝ ██║███████║██████╔╝{Style.RESET_ALL}
+{Fore.MAGENTA + Style.BRIGHT}╚═╝  ╚═══╝ ╚═════╝ ╚══════╝ ╚═╝     ╚═╝╚══════╝╚═════╝{Style.RESET_ALL}
 """
